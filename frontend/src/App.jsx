1061beeb6070b68ed294018af5e8b565ebc4c0e2
GuiPich <110974962+GuiPich@users.noreply.github.com>
2023-01-02 10:36:39 +0100

    complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
 import Home from "./pages/Home";
+import { useRef } from "react";
+import axios from "axios";
 
 import "./App.css";
 
 function App() {
+  const inputRef = useRef();
+
+  const hSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("avatar", inputRef.current.files[0]);
+    axios.post("http://localhost:5000/api/avatar", formData)
+  };
+
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
       <p>coucou</p>
+
+      <form enctype="multipart/form-data" onSubmit={hSubmit}>
+        <input type="file" name="monfichier" ref={inputRef} />
+        <button type="submit">Envoyer</button>
+      </form>
     </div>
   );
 }
